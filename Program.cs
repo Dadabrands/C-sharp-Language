eb7a49178dadc7e1ad211c4a50f388efd03a034a
Dadabrands <Dadajoshua055@gmail.com>
2023-08-28 12:55:26 +0100

    ShowRuntime and Compile time Error

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ShowCompileRuntimeErrors
+  {
+    class Program
+    {
+     static void Main(string[] args)
+        {
+        //    int integer;
+        //    integer = 12;
+       
+       int int1 = 1;
+       int int2 = 0;
+       int int3 = int1 * int2;
+
+
+        }
+    }
+  }  
---
+++ ShowCompileRuntimeErrors/ShowCompileRuntimeErrors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ShowCompileRuntimeErrors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+
+//             Compile-time errors and runtime errors are two distinct types of errors that can occur in a program, each happening at a different stage of the software development and execution process.
+
+// Compile-Time Error:
+// A compile-time error occurs during the process of translating the human-readable source code into machine-executable code. These errors are detected by the compiler before the program is actually executed. They typically result from violations of the language syntax or semantics. Some common causes of compile-time errors include:
+
+// Syntax errors: Incorrect usage of language constructs, missing or mismatched brackets, incorrect punctuation, etc.
+// Type errors: Assigning values of incompatible data types, using undefined variables, and so on.
+// Semantic errors: Violating language rules or logic, such as using a variable before it's defined.
+// Compile-time errors prevent the program from successfully being compiled into executable code. The program cannot proceed to the execution stage until these errors are fixed. Examples of compile-time errors include missing semicolons, undeclared variables, and using the wrong number of arguments in a function call.
+
+// Runtime Error:
+// A runtime error occurs when a program is executing and encounters an unexpected condition or behavior that was not caught by the compiler. These errors only become apparent while the program is running. Runtime errors are often caused by factors that can't be determined during the compilation process, such as incorrect user input, external resources not being available, or unanticipated data values.
+
+// Common examples of runtime errors include:
+
+// Division by zero: Attempting to divide a number by zero.
+// Null reference: Trying to access a method or property of an object that is null.
+// Array index out of bounds: Accessing an array element with an index that is outside the valid range.
+// File not found: Trying to open or read a file that doesn't exist.
+// Runtime errors can lead to program crashes, unexpected behavior, or incorrect results. Exception handling mechanisms are used to catch and manage these errors, allowing the program to gracefully handle exceptional situations and possibly recover without terminating.
+
+// In summary, compile-time errors are detected by the compiler during the code translation process, preventing the program from being compiled. Runtime errors occur while the program is running, resulting from unexpected conditions that were not caught during compilation. Both types of errors require careful debugging and handling to ensure the correctness and reliability of the software.
+        }
+    }
+}
